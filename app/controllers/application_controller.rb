fc7d04d1838c904753acda5fb79593c35eb8fc13
Walter M <waltermccone88@gmail.com>
2021-10-22 11:45:18 -0500

    Add user restrictions to the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to access"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   #Will set the article for our main CRUD methods
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except:[:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     
@@ -56,6 +58,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 
 end
