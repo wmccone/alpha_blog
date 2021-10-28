65148142532b162545223bf8e2bdf803c47046cb
Walter M <waltermccone88@gmail.com>
2021-10-28 16:46:13 -0500

    Build base controller and views. Scaffold functional test for the controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ resources :users, except: [:new]
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+resources :categories, except: [:destroy]
 end
