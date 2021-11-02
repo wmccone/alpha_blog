dfe6fd05f77043ff040c8b926b2d410a0125ff9c
Walter M <waltermccone88@gmail.com>
2021-11-02 13:29:26 -0500

    Create a many to many associations table for article_categories and create the many to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_214237) do
+ActiveRecord::Schema.define(version: 2021_11_02_181414) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
